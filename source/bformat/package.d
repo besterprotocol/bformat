1f7cb1180421e701d978a6c661ba4d2d70f59ca3
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-03-25 21:39:50 +0200

    Package (bformat)
    - Documented both imports seperately

--- source/bformat/package.d
+++ source/bformat/package.d
@@ -1,3 +1,15 @@
 module bformat;
 
-public import bformat.sockets : sendMessage, receiveMessage;
+/**
+ * Encodes the provided message into the bformat format
+ * and sends it over the provided socket
+ */
+public import bformat.sockets : sendMessage;
+
+/**
+ * Receives a message from the provided socket
+ * by decoding the streamed bytes into bformat
+ * and finally placing the resulting payload in
+ * the provided array
+ */
+public import bformat.sockets : receiveMessage;
